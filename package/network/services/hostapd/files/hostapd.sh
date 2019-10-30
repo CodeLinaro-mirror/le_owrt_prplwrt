9c3c021478e1995698d67f2229d0ba967a92df1e
John Crispin <john@phrozen.org>
2019-10-30 16:57:22 +0100

    hostapd: add ubus reload
    Add ubus interface to hostapd and wpa_supplicant to allow dynamically
    reloading wiface configuration without having to restart the hostapd
    process.
    As a consequence, both hostapd and wpa_supplicant are now started
    persistently on boot for each wifi device in the system and then
    receive ubus calls adding, modifying or removing interface
    configuration.
    At a later stage it would be desirable to reduce the services to one
    single instance managing all radios.
    
    Signed-off-by: John Crispin <john@phrozen.org>
    Signed-off-by: Daniel Golle <daniel@makrotopia.org>
    [rme: adapt PKG_RELEASE, refresh patches]
    Signed-off-by: Raphaël Mélotte <raphael.melotte@mind.be>
    (cherry picked from commit 60fb4c92b6b0d1582d31e02167b90b424185f3a2)
    (cherry picked from commit 7578a48256f860f335bf504d8715684e310e30a9)

--- package/network/services/hostapd/files/hostapd.sh
+++ package/network/services/hostapd/files/hostapd.sh
@@ -993,21 +993,19 @@ EOF
 }
 
 wpa_supplicant_run() {
-	local ifname="$1"; shift
+	local ifname="$1"
+	local hostapd_ctrl="$2"
 
 	_wpa_supplicant_common "$ifname"
 
-	/usr/sbin/wpa_supplicant -B -s \
-		${network_bridge:+-b $network_bridge} \
-		-P "/var/run/wpa_supplicant-${ifname}.pid" \
-		-D ${_w_driver:-wext} \
-		-i "$ifname" \
-		-c "$_config" \
-		-C "$_rpath" \
-		"$@"
+	ubus call wpa_supplicant.$phy config_add "{ \
+		\"driver\": \"${_w_driver:-wext}\", \"ctrl\": \"$_rpath\", \
+		\"iface\": \"$ifname\", \"config\": \"$_config\" \
+		${network_bridge:+, \"bridge\": \"$network_bridge\"} \
+		${hostapd_ctrl:+, \"hostapd_ctrl\": \"$hostapd_ctrl\"} \
+		}"
 
 	ret="$?"
-	wireless_add_process "$(cat "/var/run/wpa_supplicant-${ifname}.pid")" /usr/sbin/wpa_supplicant 1
 
 	[ "$ret" != 0 ] && wireless_setup_vif_failed WPA_SUPPLICANT_FAILED
 
@@ -1015,5 +1013,5 @@ wpa_supplicant_run() {
 }
 
 hostapd_common_cleanup() {
-	killall hostapd wpa_supplicant meshd-nl80211
+	killall meshd-nl80211
 }
