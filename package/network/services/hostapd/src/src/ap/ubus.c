16a3e514f803da64b9ae6924e207c559928eb99e
Daniel Golle <daniel@makrotopia.org>
2020-04-06 23:58:40 +0100

    hostapd: reduce to a single instance per service
    Signed-off-by: Daniel Golle <daniel@makrotopia.org>
    [rme: fix conflicts, refresh patches]
    Signed-off-by: Raphaël Mélotte <raphael.melotte@mind.be>
    (cherry picked from commit f37d6342367b4f8393f9693edd96b12dfa016399)

--- package/network/services/hostapd/src/src/ap/ubus.c
+++ package/network/services/hostapd/src/src/ap/ubus.c
@@ -1110,21 +1110,12 @@ static struct ubus_object_type daemon_object_type =
 void hostapd_ubus_add(struct hapd_interfaces *interfaces)
 {
 	struct ubus_object *obj = &interfaces->ubus;
-	int name_len;
 	int ret;
 
 	if (!hostapd_ubus_init())
 		return;
 
-	name_len = strlen("hostapd") + 1;
-	if (interfaces->name)
-		name_len += strlen(interfaces->name) + 1;
-	obj->name = malloc(name_len);
-	strcpy(obj->name, "hostapd");
-	if (interfaces->name) {
-		strcat(obj->name, ".");
-		strcat(obj->name, interfaces->name);
-	}
+	obj->name = strdup("hostapd");
 
 	obj->type = &daemon_object_type;
 	obj->methods = daemon_object_type.methods;
