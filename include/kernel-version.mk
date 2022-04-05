5fda51b82c62954ab1d8df972212eba663305da7
Petr Štetiar <ynezz@true.cz>
2022-04-05 14:20:05 +0200

    kernel: bump 4.14 to 4.14.284
    All patches refreshed automagically, just following two patches needed
    manual rebasing:
    
     ath79: 403-mtd_fix_cfi_cmdset_0002_status_check.patch
     ar71xx: 403-mtd_fix_cfi_cmdset_0002_status_check.patch
    
    Signed-off-by: Petr Štetiar <ynezz@true.cz>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -6,9 +6,9 @@ ifdef CONFIG_TESTING_KERNEL
   KERNEL_PATCHVER:=$(KERNEL_TESTING_PATCHVER)
 endif
 
-LINUX_VERSION-4.14 = .275
+LINUX_VERSION-4.14 = .284
 
-LINUX_KERNEL_HASH-4.14.275 = 100a9960fb2d8e079c9feeef640715a7fb749ed728a57e427f9e2443212e58f9
+LINUX_KERNEL_HASH-4.14.284 = 9852cca0a93111979ae5d372d2592d9224576d80c6d85781736419a5638bebb8
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
