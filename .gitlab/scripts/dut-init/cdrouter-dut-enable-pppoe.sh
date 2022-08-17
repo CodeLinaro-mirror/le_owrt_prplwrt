3a1755ce7b8742c1876fae240d675f37c3096443
Matthias FRANCK <matthias.franck_ext@softathome.com>
2022-08-17 17:16:47 +0200

    Adapt PPP CDRouter init script

--- .gitlab/scripts/dut-init/cdrouter-dut-enable-pppoe.sh
+++ .gitlab/scripts/dut-init/cdrouter-dut-enable-pppoe.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 ssh root@$TARGET_LAN_IP "ubus -t 60 wait_for PPP"
+ssh root@$TARGET_LAN_IP "ubus -t 60 wait_for DHCPv4"
+ssh root@$TARGET_LAN_IP "ubus-cli DHCPv4.Client.1.Enable=0"
 ssh root@$TARGET_LAN_IP "ubus call PPP.Interface.1 _set '{\"parameters\":{\"Enable\":1}}'"
