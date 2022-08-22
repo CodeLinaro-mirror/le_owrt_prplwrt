b6378a3a4bc6e028d39c15c7c6ad22c2096a181e
Matthias Franck <7363593-Matthias_franck@users.noreply.gitlab.com>
2022-08-22 15:19:24 +0000

    Update cdrouter-dut-enable-pppoe.sh

--- .gitlab/scripts/dut-init/cdrouter-dut-enable-pppoe.sh
+++ .gitlab/scripts/dut-init/cdrouter-dut-enable-pppoe.sh
@@ -8,3 +8,7 @@ ssh root@$TARGET_LAN_IP "ubus call DHCPv4.Client.1 _set '{\"parameters\":{\"Enab
 ssh root@$TARGET_LAN_IP "ubus call PPP.Interface.1 _set '{\"parameters\":{\"Enable\":1}}'"
 ssh root@$TARGET_LAN_IP "ubus call IP.Interface.2 _set '{\"parameters\":{\"LowerLayers\":\"Device.PPP.Interface.1.\"}}'"
 ssh root@$TARGET_LAN_IP "ubus call IP.Interface.2.IPv4Address.1 _set '{\"parameters\":{\"AddressingType\":\"IPCP\"}}'"
+sleep 20
+ssh root@$TARGET_LAN_IP "ubus call IP.Interface.2 _get"
+ssh root@$TARGET_LAN_IP "ubus call PPP.Interface.1 _get"
+ssh root@$TARGET_LAN_IP "ubus list"
