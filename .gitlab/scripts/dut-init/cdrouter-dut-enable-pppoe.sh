1223d06507ddda380323dcd3de26a853275b676f
Matthias FRANCK <matthias.franck_ext@softathome.com>
2022-08-22 11:14:25 +0200

    Correct startup script for ppp tests

--- .gitlab/scripts/dut-init/cdrouter-dut-enable-pppoe.sh
+++ .gitlab/scripts/dut-init/cdrouter-dut-enable-pppoe.sh
@@ -3,4 +3,6 @@
 ssh root@$TARGET_LAN_IP "ubus -t 60 wait_for PPP"
 ssh root@$TARGET_LAN_IP "ubus -t 60 wait_for DHCPv4"
 ssh root@$TARGET_LAN_IP "ubus-cli DHCPv4.Client.1.Enable=0"
-ssh root@$TARGET_LAN_IP "ubus call PPP.Interface.1 _set '{\"parameters\":{\"Enable\":1}}'"
+ssh root@$TARGET_LAN_IP "ubus-cli PPP.Interface.1.Enable=0"
+ssh root@$TARGET_LAN_IP "ubus-cli IP.Interface.2.LowerLayers=Device.PPP.Interface.1."
+ssh root@$TARGET_LAN_IP "ubus-cli IP.Interface.2.IPv4Address.1.AddressingType=IPCP"
