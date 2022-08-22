f95a3047c3772b9f92f2b0910495467a46c6911e
Matthias FRANCK <matthias.franck_ext@softathome.com>
2022-08-22 15:08:54 +0200

    Try to figure out dns server

--- .gitlab/scripts/dut-init/cdrouter-dut-enable-pppoe.sh
+++ .gitlab/scripts/dut-init/cdrouter-dut-enable-pppoe.sh
@@ -6,5 +6,6 @@ ssh root@$TARGET_LAN_IP "ubus -t 60 wait_for IP"
 sleep 30
 ssh root@$TARGET_LAN_IP "ubus call DHCPv4.Client.1 _set '{\"parameters\":{\"Enable\":0}}'"
 ssh root@$TARGET_LAN_IP "ubus call PPP.Interface.1 _set '{\"parameters\":{\"Enable\":1}}'"
-ssh root@$TARGET_LAN_IP "ubus call  IP.Interface.2 _set '{\"parameters\":{\"LowerLayers\":\"Device.PPP.Interface.1.\"}}'"
-ssh root@$TARGET_LAN_IP "ubus call  IP.Interface.2.IPv4Address.1 _set '{\"parameters\":{\"AddressingType\":\"IPCP\"}}'"
+ssh root@$TARGET_LAN_IP "ubus call IP.Interface.2 _set '{\"parameters\":{\"LowerLayers\":\"Device.PPP.Interface.1.\"}}'"
+ssh root@$TARGET_LAN_IP "ubus call IP.Interface.2.IPv4Address.1 _set '{\"parameters\":{\"AddressingType\":\"IPCP\"}}'"
+ssh root@$TARGET_LAN_IP "ubus call PPP.Interface.1.IPCP _get"
