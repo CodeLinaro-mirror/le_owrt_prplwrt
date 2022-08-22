c85f5fa4e2596da41e05b4510a4c465fc8b839c6
Matthias FRANCK <matthias.franck_ext@softathome.com>
2022-08-22 12:54:14 +0200

    Add extra sleep in ppp init script

--- .gitlab/scripts/dut-init/cdrouter-dut-enable-pppoe.sh
+++ .gitlab/scripts/dut-init/cdrouter-dut-enable-pppoe.sh
@@ -3,6 +3,7 @@
 ssh root@$TARGET_LAN_IP "ubus -t 60 wait_for PPP"
 ssh root@$TARGET_LAN_IP "ubus -t 60 wait_for DHCPv4"
 ssh root@$TARGET_LAN_IP "ubus -t 60 wait_for IP"
+sleep 30
 ssh root@$TARGET_LAN_IP "ubus-cli DHCPv4.Client.1.Enable=0"
 ssh root@$TARGET_LAN_IP "ubus-cli PPP.Interface.1.Enable=1"
 ssh root@$TARGET_LAN_IP "ubus-cli IP.Interface.2.LowerLayers=Device.PPP.Interface.1."
