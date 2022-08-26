ae50bd250355fe53ae328b6af96305da1e6e1e69
Matthias FRANCK <matthias.franck_ext@softathome.com>
2022-08-26 16:47:59 +0200

    Correct PPP init script

--- .gitlab/scripts/dut-init/cdrouter-dut-enable-pppoe.sh
+++ .gitlab/scripts/dut-init/cdrouter-dut-enable-pppoe.sh
@@ -8,13 +8,5 @@ ssh root@$TARGET_LAN_IP "ubus call DHCPv4.Client.1 _set '{\"parameters\":{\"Enab
 ssh root@$TARGET_LAN_IP "ubus call PPP.Interface.1 _set '{\"parameters\":{\"Enable\":1}}'"
 ssh root@$TARGET_LAN_IP "ubus call IP.Interface.2 _set '{\"parameters\":{\"LowerLayers\":\"Device.PPP.Interface.1.\"}}'"
 ssh root@$TARGET_LAN_IP "ubus call IP.Interface.2.IPv4Address.1 _set '{\"parameters\":{\"AddressingType\":\"IPCP\"}}'"
-sleep 10
-ssh root@$TARGET_LAN_IP "ubus call IP.Interface.2 _get"
-ssh root@$TARGET_LAN_IP "ubus call PPP.Interface.1 _get"
 
 ssh root@$TARGET_LAN_IP "ubus call DNS.Client.Server _add '{\"parameters\":{\"Alias\":\"ppp\",\"Enable\":\"1\",\"DNSServer\":\"202.254.101.1\",\"Interface\":\"Device.IP.Interface.2.\",\"Type\":\"Static\"}}'"
-sleep 5
-ssh root@$TARGET_LAN_IP "ubus call PPP.Interface.1 _set '{\"parameters\":{\"Enable\":0}}'"
-ssh root@$TARGET_LAN_IP "ubus call PPP.Interface.1 _set '{\"parameters\":{\"Enable\":1}}'"
-sleep 5
-ssh root@$TARGET_LAN_IP "ubus call PPP.Interface.1 _get"
