87dca53f4d2e143a6e56b46982ffc17e5a3b7087
Matthias FRANCK <matthias.franck_ext@softathome.com>
2022-08-29 15:18:05 +0200

    Increase waiting time

--- .gitlab/scripts/dut-init/cdrouter-dut-enable-pppoe.sh
+++ .gitlab/scripts/dut-init/cdrouter-dut-enable-pppoe.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-ssh root@$TARGET_LAN_IP "ubus -t 60 wait_for PPP"
-ssh root@$TARGET_LAN_IP "ubus -t 60 wait_for DHCPv4"
-ssh root@$TARGET_LAN_IP "ubus -t 60 wait_for IP"
+ssh root@$TARGET_LAN_IP "ubus -t 200 wait_for PPP"
+ssh root@$TARGET_LAN_IP "ubus -t 200 wait_for DHCPv4"
+ssh root@$TARGET_LAN_IP "ubus -t 200 wait_for IP"
 sleep 15
 ssh root@$TARGET_LAN_IP "ubus call DHCPv4.Client.1 _set '{\"parameters\":{\"Enable\":0}}'"
 ssh root@$TARGET_LAN_IP "ubus call PPP.Interface.1 _set '{\"parameters\":{\"Enable\":1}}'"
