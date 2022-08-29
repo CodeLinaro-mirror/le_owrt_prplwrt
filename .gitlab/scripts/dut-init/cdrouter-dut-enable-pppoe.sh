2393192c614cb5e67f4f6fedd75488d029ed2a3e
Matthias FRANCK <matthias.franck_ext@softathome.com>
2022-08-29 20:38:28 +0200

    PROVISORY: ci: cdrouter-dut-enable-ppoe: Correct init script for ppp tests
    As WAN manager has currently no support for PPP WAN uplink, we need to
    prepare the PPP WAN uplink manually.
    
    References: PCF-692
    Signed-off-by: Matthias FRANCK <matthias.franck_ext@softathome.com>
    Signed-off-by: Petr Štetiar <ynezz@true.cz> [commit description]

--- .gitlab/scripts/dut-init/cdrouter-dut-enable-pppoe.sh
+++ .gitlab/scripts/dut-init/cdrouter-dut-enable-pppoe.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-ssh root@$TARGET_LAN_IP "ubus -t 60 wait_for PPP"
+ssh root@$TARGET_LAN_IP "ubus -t 200 wait_for PPP"
+ssh root@$TARGET_LAN_IP "ubus -t 200 wait_for DHCPv4"
+ssh root@$TARGET_LAN_IP "ubus -t 200 wait_for IP"
+ssh root@$TARGET_LAN_IP "ubus call DHCPv4.Client.1 _set '{\"parameters\":{\"Enable\":0}}'"
 ssh root@$TARGET_LAN_IP "ubus call PPP.Interface.1 _set '{\"parameters\":{\"Enable\":1}}'"
+ssh root@$TARGET_LAN_IP "ubus call IP.Interface.2 _set '{\"parameters\":{\"LowerLayers\":\"Device.PPP.Interface.1.\"}}'"
+ssh root@$TARGET_LAN_IP "ubus call IP.Interface.2.IPv4Address.1 _set '{\"parameters\":{\"AddressingType\":\"IPCP\"}}'"
