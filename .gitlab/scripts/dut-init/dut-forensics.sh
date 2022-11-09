ccfe9972069b16765e9798f91df52219df5cd1d7
Vincent Harlé <vincent.harle@softathome.com>
2022-11-09 18:31:54 +0100

    add succsess return to init script

--- .gitlab/scripts/dut-init/dut-forensics.sh
+++ .gitlab/scripts/dut-init/dut-forensics.sh
@@ -5,8 +5,12 @@ SCRIPT=$(curl -s http://kashyyyk.mooo.com/fw/scripts/prpl-forensic-script.sh)
 
 #loosely check if retrieval was ok or if we got an html error code
 if [ $(echo "$SCRIPT" | grep "<html>") ] ; then 
-	exit
+	echo "Warning : could not fetch forensic script"
+	exit 0
 fi
 
 #connect through ssh and play script on target
 echo "$SCRIPT" | ssh root@$TARGET_LAN_IP "/bin/sh -s" --
+
+#continue testing whatever the result is
+true
