4febd88030ff34674afb017cc718a5776b713029
Raphaël Mélotte <raphael.melotte@mind.be>
2022-11-23 15:22:15 +0100

    config: make ccache dirs configurable
    When using CCACHE, it can be useful to be able to specify a custom
    ccache directory outside of the build/staging directories. For example
    one might want to share the cache between multiple users, or want to
    re-use the same cache after cleaning the build/staging directories.
    
    For this, two new optional config options are introduced: one for the
    target builds, and one for the host builds.
    If CONFIG_CCACHE is not set, the behavior remains the same as before:
    CCACHE_DIR is not exported.
    When CONFIG_CCACHE is set, the exported ccache directory is either the
    same as before (when the new config options are not set) or the custom
    directory specified by the user (when the corresponding new option is
    set).
    
    Signed-off-by: Raphaël Mélotte <raphael.melotte@mind.be>

--- include/package.mk
+++ include/package.mk
@@ -173,7 +173,7 @@ define Build/Exports/Default
   $(1) : export CONFIG_SITE:=$$(CONFIG_SITE)
   $(1) : export PKG_CONFIG_PATH:=$$(PKG_CONFIG_PATH)
   $(1) : export PKG_CONFIG_LIBDIR:=$$(PKG_CONFIG_PATH)
-  $(if $(CONFIG_CCACHE),$(1) : export CCACHE_DIR:=$(STAGING_DIR)/ccache)
+  $(if $(CONFIG_CCACHE),$(1) : export CCACHE_DIR:=$(if $(CONFIG_CCACHE_TARGET_DIR),$(call qstrip,$(CONFIG_CCACHE_TARGET_DIR)),$(STAGING_DIR_HOST)/ccache))
 endef
 Build/Exports=$(Build/Exports/Default)
 
