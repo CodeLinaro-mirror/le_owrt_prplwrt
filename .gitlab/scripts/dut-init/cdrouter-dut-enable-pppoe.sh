d3d25c60dd3c7c2814334e1fcf498c6853e1d8ed
Vincent Harlé <vincent.harle@softathome.com>
2023-01-30 12:06:10 +0100

    virtual terminal

--- .gitlab/scripts/dut-init/cdrouter-dut-enable-pppoe.sh
+++ .gitlab/scripts/dut-init/cdrouter-dut-enable-pppoe.sh
@@ -12,4 +12,4 @@ ssh root@$TARGET_LAN_IP "ubus call IP.Interface.2.IPv4Address.1 _set '{\"paramet
 ssh root@$TARGET_LAN_IP "exec <&- >&- 2>&- ; while true; do sh -c \"date; nslookup acs-download.qacafe.com \">> /etc/tmpnslookup;  sleep 10;  done &" 
 
 #set a process to take getdebug when PPP is established 
-ssh root@$TARGET_LAN_IP 'exec <&- >&- 2>&- ; (while [ "$(ubus call PPP.Interface.1 _get | jsonfilter -e @[*].ConnectionStatus)" !=  "Connected" ] ; do sleep 10; done ; sleep 10; /bin/getDebugInformation -a;  cp /tmp/debug_all.tar.gz /etc/) &'
+ssh -tt root@$TARGET_LAN_IP 'exec <&- >&- 2>&- ; (while [ "$(ubus call PPP.Interface.1 _get | jsonfilter -e @[*].ConnectionStatus)" !=  "Connected" ] ; do sleep 10; done ; sleep 10; /bin/getDebugInformation --all -o - > /etc/debug_ppp.txt) &'
