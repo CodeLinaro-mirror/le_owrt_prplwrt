e31419fafbaba257b96d3bcdb28dfc7b65d9a0ae
Vincent Harlé <vincent.harle@softathome.com>
2023-01-31 12:26:27 +0100

    fix typo for dnsmasq conf retrieval

--- .gitlab/scripts/dut-init/triggered-debug.sh
+++ .gitlab/scripts/dut-init/triggered-debug.sh
@@ -8,4 +8,4 @@ ssh root@$TARGET_LAN_IP 'chmod a+x /tmp/luadumper.lua'
 ssh root@$TARGET_LAN_IP "exec <&- >&- 2>&- ; while true; do sh -c \"date; nslookup www.google.com; nslookup acs-download.qacafe.com \" >> /etc/tmpnslookups 2>&1;  sleep 2;  done &" 
 
 #set a process to take getdebug when PPP is established 
-ssh root@$TARGET_LAN_IP 'exec <&- >&- 2>&- ; (while [ "$(ubus call PPP.Interface.1 _get | jsonfilter -e @[*].ConnectionStatus)" !=  "Connected" ] ; do sleep 10; done ; sleep 10; cp /var/etc/dsmasq.conf.* /etc/ ; /bin/getDebugInformation -n -w -p -m -l -o - > /etc/debug_triggered.txt; /tmp/luadumper.lua  >>/etc/debug_triggered.txt;  ) &'
+ssh root@$TARGET_LAN_IP 'exec <&- >&- 2>&- ; (while [ "$(ubus call PPP.Interface.1 _get | jsonfilter -e @[*].ConnectionStatus)" !=  "Connected" ] ; do sleep 10; done ; sleep 10; cp /var/etc/dnsmasq.conf.* /etc/ ; /bin/getDebugInformation -n -w -p -m -l -o - > /etc/debug_triggered.txt; /tmp/luadumper.lua  >>/etc/debug_triggered.txt;  ) &'
