9fc7adc1c5113b8d79b2606ba7fddb09a24f9a6c
Vincent Harlé <vincent.harle@softathome.com>
2023-01-31 11:23:31 +0100

    Reenable nslookup monitor

--- .gitlab/scripts/dut-init/triggered-debug.sh
+++ .gitlab/scripts/dut-init/triggered-debug.sh
@@ -4,8 +4,8 @@
 scp .gitlab/scripts/luadumper.lua root@${TARGET_LAN_IP}:/tmp/
 ssh root@$TARGET_LAN_IP 'chmod a+x /tmp/luadumper.lua'
 
-#add a dns monitor
-#ssh root@$TARGET_LAN_IP "exec <&- >&- 2>&- ; while true; do sh -c \"date; nslookup acs-download.qacafe.com \">> /etc/tmpnslookup;  sleep 10;  done &" 
+#add a dns probe
+ssh root@$TARGET_LAN_IP "exec <&- >&- 2>&- ; while true; do sh -c \"date; nslookup acs-download.qacafe.com \" >> /etc/tmpnslookup 2>&1;  sleep 2;  done &" 
 
 #set a process to take getdebug when PPP is established 
 ssh root@$TARGET_LAN_IP 'exec <&- >&- 2>&- ; (while [ "$(ubus call PPP.Interface.1 _get | jsonfilter -e @[*].ConnectionStatus)" !=  "Connected" ] ; do sleep 10; done ; sleep 10; /bin/getDebugInformation -n -w -p -m -l -o - > /etc/debug_triggered.txt; /tmp/luadumper.lua  >>/etc/debug_triggered.txt ) &'
