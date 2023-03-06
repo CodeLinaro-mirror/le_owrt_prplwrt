17a03e5b824782509e46e3900e4296974eccc838
Petr Štetiar <ynezz@true.cz>
2023-03-06 18:32:43 +0100

    scripts: gen_config.py: disable stdout output buffering
    Python3 line buffers stdout which makes script output confusing so lets
    disable it.
    
    Signed-off-by: Petr Štetiar <ynezz@true.cz>

--- scripts/gen_config.py
+++ scripts/gen_config.py
@@ -3,10 +3,12 @@
 import yaml
 from pathlib import Path
 from shutil import rmtree
+import io
 import sys
 from subprocess import run
 from os import getenv
 
+sys.stdout = io.TextIOWrapper(open(sys.stdout.fileno(), "wb", 0), write_through=True)
 profile_folder = Path(getenv("PROFILES", "./profiles"))
 
 
