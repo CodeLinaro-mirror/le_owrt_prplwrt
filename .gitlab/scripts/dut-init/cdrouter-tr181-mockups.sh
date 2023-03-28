717267df253f4a18e9fea431c851bf67a1910559
Mouna ALOUI <mouna.aloui_ext@softathome.com>
2023-03-28 13:40:55 +0000

    update startup scripts

--- .gitlab/scripts/dut-init/cdrouter-tr181-mockups.sh
+++ .gitlab/scripts/dut-init/cdrouter-tr181-mockups.sh
@@ -3,4 +3,3 @@
 scp .gitlab/mockups/tr181-mockups.tar.gz root@${TARGET_LAN_IP}:/tmp/
 ssh root@$TARGET_LAN_IP "tar xzf /tmp/tr181-mockups.tar.gz -C / && /etc/init.d/tr181-mockups start"
 ssh root@$TARGET_LAN_IP "ubus call ProxyManager register '{\"proxy\":\"Device.LANConfigSecurity.\", \"real\":\"LANConfigSecurity.\" } '"
-ssh root@$TARGET_LAN_IP "ubus call ProxyManager register '{\"proxy\":\"Device.CWMPManagementServer.\", \"real\":\"ManagementServer.\" } '"
