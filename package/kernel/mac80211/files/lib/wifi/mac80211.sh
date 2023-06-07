shng5175/prpl

mac80211: rename default SSID to prplOS and add prplOS-guest SSID
It was decided, that prplOS should ship with guest bridge in default
config, so this adds prplOS-guest SSID in guest bridge. While at it,
rename SSID from default OpenWrt to prplOS.

Signed-off-by: Petr Štetiar <ynezz@true.cz>

package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -119,8 +119,15 @@ detect_mac80211() {
 			set wireless.default_radio${devidx}.device=radio${devidx}
 			set wireless.default_radio${devidx}.network=lan
 			set wireless.default_radio${devidx}.mode=ap
-			set wireless.default_radio${devidx}.ssid=OpenWrt
+			set wireless.default_radio${devidx}.ssid=prplOS
 			set wireless.default_radio${devidx}.encryption=none
+
+			set wireless.guest_radio${devidx}=wifi-iface
+			set wireless.guest_radio${devidx}.device=radio${devidx}
+			set wireless.guest_radio${devidx}.network=guest
+			set wireless.guest_radio${devidx}.mode=ap
+			set wireless.guest_radio${devidx}.ssid=prplOS-guest
+			set wireless.guest_radio${devidx}.encryption=none
 EOF
 		uci -q commit wireless
 

CodeLinaro-mirror/le_owrt_prplwrt

(cherry picked from commit 0cc302f744e7ad59c8773fac36f139dc174b6127)

package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -209,7 +209,14 @@ detect_mac80211() {
 			set wireless.default_${name}.device=${name}
 			set wireless.default_${name}.network=lan
 			set wireless.default_${name}.mode=ap
-			set wireless.default_${name}.ssid=OpenWrt
+			set wireless.default_${name}.ssid=prplOS
+			set wireless.default_${name}.encryption=none
+
+			set wireless.default_${name}=wifi-iface
+			set wireless.default_${name}.device=${name}
+			set wireless.default_${name}.network=lan
+			set wireless.default_${name}.mode=ap
+			set wireless.default_${name}.ssid=prplOS-guest
 			set wireless.default_${name}.encryption=none
 EOF
 		uci -q commit wireless