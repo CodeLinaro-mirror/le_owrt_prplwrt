36efaf269f031fcda4d4c084af74662aeddb47b8
Marouan MCHARFI <marouan.mcharfi_ext@softathome.com>
2023-09-05 19:31:42 +0200

    WIP: Scripts: CDRouter: change the AP in setup wireless
    Now the AP 2 is associated to 2.4GHz radio. Need to use this
    AP for CD router test
    
    Signed-off-by: Marouan MCHARFI <marouan.mcharfi_ext@softathome.com>

--- .gitlab/scripts/dut-init/cdrouter-dut-configure-wireless.sh
+++ .gitlab/scripts/dut-init/cdrouter-dut-configure-wireless.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ssh "root@$TARGET_LAN_IP" "ubus -t 200 wait_for WiFi.AccessPoint.1"
-ssh "root@$TARGET_LAN_IP" "ubus -S call WiFi.AccessPoint.1 _set '{\"parameters\":{\"Enable\":1}}'"
+ssh "root@$TARGET_LAN_IP" "ubus -t 200 wait_for WiFi.AccessPoint.2"
+ssh "root@$TARGET_LAN_IP" "ubus -S call WiFi.AccessPoint.2 _set '{\"parameters\":{\"Enable\":1}}'"
