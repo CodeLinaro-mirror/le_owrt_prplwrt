7a52da49db2b1da4e3050fe2fb2713717b6a8f27
Matthias FRANCK <matthias.franck_ext@softathome.com>
2023-09-27 09:40:30 +0200

    .gitlab: scripts: Update scripts for M1 delivery
    - cdrouter-dut-enable-pppoe: Update the script to use wanmanager to set PPP mode wan mode.
    - testbed-device.py: Update commands to work with latest datamodel changes. At the same time update some commands to use ubus-cli rather than ubus directly.
    
    Signed-off-by: Matthias FRANCK <matthias.franck_ext@softathome.com>

--- .gitlab/scripts/dut-init/cdrouter-dut-enable-pppoe.sh
+++ .gitlab/scripts/dut-init/cdrouter-dut-enable-pppoe.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-ssh root@$TARGET_LAN_IP "ubus -t 200 wait_for PPP"
-ssh root@$TARGET_LAN_IP "ubus -t 200 wait_for DHCPv4"
-ssh root@$TARGET_LAN_IP "ubus -t 200 wait_for IP"
-ssh root@$TARGET_LAN_IP "ubus call DHCPv4.Client.1 _set '{\"parameters\":{\"Enable\":0}}'"
-ssh root@$TARGET_LAN_IP "ubus call PPP.Interface.1 _set '{\"parameters\":{\"Enable\":1}}'"
-ssh root@$TARGET_LAN_IP "ubus call IP.Interface.2 _set '{\"parameters\":{\"LowerLayers\":\"Device.PPP.Interface.1.\"}}'"
-ssh root@$TARGET_LAN_IP "ubus call IP.Interface.2.IPv4Address.1 _set '{\"parameters\":{\"AddressingType\":\"IPCP\"}}'"
+ssh root@$TARGET_LAN_IP "ubus -t 200 wait_for WANManager"
+ssh root@$TARGET_LAN_IP "ubus-cli WANManager.WAN.Ethernet_PPP.Intf.1.Type=\"untagged\""
+ssh root@$TARGET_LAN_IP "ubus call WANManager setWANMode '{ \"WANMode\": \"Ethernet_PPP\" }'"
