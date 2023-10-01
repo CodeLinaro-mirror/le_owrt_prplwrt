e5576b4103269db39ecd2d89f4e61e855a44033c
Petr Štetiar <ynezz@true.cz>
2023-10-01 06:00:32 +0000

    version.mk: introduce prplOS Git based versioning
    Henceforth, we will utilize the prplOS version throughout the
    development cycle, determined by the output of the `git describe`
    command. The version will be composed of:
    
     - The fixed version number of the upcoming release currently in
       development.
    
     - The abbreviated (8 characters) object hash of the most recent commit.
    
    For instance, with the ongoing development of prplOS version 3.0.0 and
    having the Git hash `5a2a69d`, the resulting version would be
    `3.0.0-5a2a69d`.
    
    References: PCF-691
    
    Signed-off-by: Petr Štetiar <ynezz@true.cz>

--- include/version.mk
+++ include/version.mk
@@ -22,8 +22,10 @@ PKG_CONFIG_DEPENDS += \
 
 sanitize = $(call tolower,$(subst _,-,$(subst $(space),-,$(1))))
 
+VERSION_PRPLOS:=$(if $(VERSION_PRPLOS),$(VERSION_PRPLOS),$(shell $(TOPDIR)/scripts/prplos_version.sh))
+
 VERSION_NUMBER:=$(call qstrip,$(CONFIG_VERSION_NUMBER))
-VERSION_NUMBER:=$(if $(VERSION_NUMBER),$(VERSION_NUMBER),23.05.3)
+VERSION_NUMBER:=$(if $(VERSION_NUMBER),$(VERSION_NUMBER),$(VERSION_PRPLOS))
 
 VERSION_CODE:=$(call qstrip,$(CONFIG_VERSION_CODE))
 VERSION_CODE:=$(if $(VERSION_CODE),$(VERSION_CODE),r23809-234f1a2efa)
