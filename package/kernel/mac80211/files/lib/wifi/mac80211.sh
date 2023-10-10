812d8c149d6d79ed2aaff59e1de581bb2e2e591d
Petr Štetiar <ynezz@true.cz>
2023-10-10 09:21:01 +0000

    mac80211: fix default SSIDs back to prplOS and prplOS-guest
    In commit f0a6969d3de8 ("prplOS: mac80211: backport 6.1.24 version") the
    default prplOS/prplOS-guest SSIDs introduced in commit 23dd4f0dcc55
    ("mac80211: rename default SSID to prplOS and add prplOS-guest SSID")
    got broken, so lets fix it.
    
    Fixes: f0a6969d3de8 ("prplOS: mac80211: backport 6.1.24 version")
    Signed-off-by: Petr Štetiar <ynezz@true.cz>
    (cherry picked from commit 991147c863be3ca9d7a0f1a5eae7da5b3350fc8e)

--- package/kernel/mac80211/files/lib/wifi/mac80211.sh
+++ package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -209,8 +209,15 @@ detect_mac80211() {
 			set wireless.default_${name}.device=${name}
 			set wireless.default_${name}.network=lan
 			set wireless.default_${name}.mode=ap
-			set wireless.default_${name}.ssid=OpenWrt
+			set wireless.default_${name}.ssid=prplOS
 			set wireless.default_${name}.encryption=none
+
+			set wireless.guest_${name}=wifi-iface
+			set wireless.guest_${name}.device=${name}
+			set wireless.guest_${name}.network=guest
+			set wireless.guest_${name}.mode=ap
+			set wireless.guest_${name}.ssid=prplOS-guest
+			set wireless.guest_${name}.encryption=none
 EOF
 		uci -q commit wireless
 	done
