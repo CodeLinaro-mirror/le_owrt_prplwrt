CodeLinaro-mirror/le_owrt_prplwrt

commit 23dd4f0dcc55e875dd94c2d87360c62dbde74a11

mac80211: rename default SSID to prplOS and add prplOS-guest SSID
It was decided, that prplOS should ship with guest bridge in default
config, so this adds prplOS-guest SSID in guest bridge. While at it,
rename SSID from default OpenWrt to prplOS.

Signed-off-by: Petr Štetiar <ynezz@true.cz>
(cherry picked from commit 0cc302f744e7ad59c8773fac36f139dc174b6127)

package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -180,8 +180,15 @@ detect_mac80211() {
 			set wireless.default_radio${devidx}.device=radio${devidx}
 			set wireless.default_radio${devidx}.network=lan
 			set wireless.default_radio${devidx}.mode=ap
-			set wireless.default_radio${devidx}.ssid=OpenWrt
+			set wireless.default_radio${devidx}.ssid=prplOS
 			set wireless.default_radio${devidx}.encryption=none
+
+			set wireless.guest_radio${devidx}=wifi-iface
+			set wireless.guest_radio${devidx}.device=radio${devidx}
+			set wireless.guest_radio${devidx}.network=guest
+			set wireless.guest_radio${devidx}.mode=ap
+			set wireless.guest_radio${devidx}.ssid=prplOS-guest
+			set wireless.guest_radio${devidx}.encryption=none
 EOF
 		uci -q commit wireless
 

commit 991147c863be3ca9d7a0f1a5eae7da5b3350fc8e

mac80211: fix default SSIDs back to prplOS and prplOS-guest
In commit f0a6969d3de8 ("prplOS: mac80211: backport 6.1.24 version") the
default prplOS/prplOS-guest SSIDs introduced in commit 23dd4f0dcc55
("mac80211: rename default SSID to prplOS and add prplOS-guest SSID")
got broken, so lets fix it.

Fixes: f0a6969d3de8 ("prplOS: mac80211: backport 6.1.24 version")
Signed-off-by: Petr Štetiar <ynezz@true.cz>

package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -209,8 +209,15 @@ detect_mac80211() {
 			set wireless.default_${name}.device=${name}
 			set wireless.default_${name}.network=lan
 			set wireless.default_${name}.mode=ap
-			set wireless.default_${name}.ssid=OpenWrt
+			set wireless.default_${name}.ssid=prplOS
 			set wireless.default_${name}.encryption=none
+
+			set wireless.guest_${name}=wifi-iface
+			set wireless.guest_${name}.device=${name}
+			set wireless.guest_${name}.network=guest
+			set wireless.guest_${name}.mode=ap
+			set wireless.guest_${name}.ssid=prplOS-guest
+			set wireless.guest_${name}.encryption=none
 EOF
 		uci -q commit wireless
 	done