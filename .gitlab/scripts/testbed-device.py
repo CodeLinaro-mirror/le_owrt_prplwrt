CodeLinaro-mirror/le_owrt_prplwrt

commit 7a52da49db2b1da4e3050fe2fb2713717b6a8f27

.gitlab: scripts: Update scripts for M1 delivery
- cdrouter-dut-enable-pppoe: Update the script to use wanmanager to set PPP mode wan mode.
- testbed-device.py: Update commands to work with latest datamodel changes. At the same time update some commands to use ubus-cli rather than ubus directly.

Signed-off-by: Matthias FRANCK <matthias.franck_ext@softathome.com>

.gitlab/scripts/testbed-device.py
@@ -130,11 +130,10 @@ class TestbedDevice:
             self.init_swconfig_glinet()
 
     def _init_wan_vlan(self):
-        self.shell.run("ubus -t 60 wait_for X_PRPL-COM_WANManager.WAN")
-        self.ubus_tr181.set("X_PRPL-COM_WANManager.WAN.2.Intf.1", {"VlanID": 101})
-        self.ubus_tr181.call(
-            "X_PRPL-COM_WANManager", "setWANMode", {"WANMode": "demo_vlanmode"}
-        )
+        self.shell.run("ubus -t 60 wait_for WANManager.WAN")
+        self.shell.run("ubus-cli WANManager.WAN.2.Intf.1.VlanID=101")
+        self.shell.run("ubus-cli WANManager.WAN.2.Intf.2.VlanID=100")
+        self.shell.run("ubus call WANManager setWANMode '{ \"WANMode\": \"Ethernet_DHCP\" }'")
 
     def _init_lan_vlan(self):
         bridge_lan_ports = {
@@ -146,30 +145,14 @@ class TestbedDevice:
             self.board_name, "Bridging.Bridge.1.Port.2"
         )
 
-        self.ubus_tr181.set("Bridging.Bridge.1", {"Standard": "802.1Q-2005"})
-        self.ubus_tr181.add(
-            "Bridging.Bridge.1.VLAN",
-            {"Alias": "vlan201", "Name": "vlan201", "VLANID": 201, "Enable": 1},
-        )
-        self.ubus_tr181.set(
-            f"{bridge_vlan_port}",
-            {
-                "AcceptableFrameTypes": "AdmitOnlyVLANTagged",
-                "PVID": "201",
-                "Type": "CustomerVLANPort",
-                "Enable": 1,
-            },
-        )
-        self.ubus_tr181.add(
-            "Bridging.Bridge.1.VLANPort",
-            {
-                "Alias": "LAN",
-                "Name": "vlan201",
-                "Port": f"Device.{bridge_vlan_port}.",
-                "VLAN": "Device.Bridging.Bridge.1.VLAN.1.",
-                "Enable": 1,
-            },
-        )
+        self.shell.run("ubus-cli Bridging.Bridge.1.Standard=\"802.1Q-2005\"")
+        self.shell.run("ubus-cli Bridging.Bridge.1.VLAN.+{Alias='vlan201', Name='vlan201', VLANID=201, Enable=1}")
+        self.shell.run("ubus-cli Bridging.Bridge.1.Port.6.AcceptableFrameTypes=\"AdmitOnlyVLANTagged\"")
+        self.shell.run("ubus-cli Bridging.Bridge.1.Port.6.PVID=\"201\"")
+        self.shell.run("ubus-cli Bridging.Bridge.1.Port.6.Type=\"CustomerVLANPort\"")
+        self.shell.run("ubus-cli Bridging.Bridge.1.Port.6.Enable=1")
+
+        self.shell.run("ubus-cli Bridging.Bridge.1.VLANPort+{Alias='LAN', Name='vlan201', Port='Device.Bridging.Bridge.1.Port.6.', VLAN='Device.Bridging.Bridge.1.VLAN.1.', Enable=1}")
 
     def init_vlans(self):
         self.init_shell()
@@ -192,7 +175,7 @@ class TestbedDevice:
         )
         time.sleep(15)
 
-        self.ubus_tr181.get("Ethernet.VLANTermination")
+        self.shell.run("ubus-cli Ethernet.VLANTermination.?")
         self.shell.run("(cat /proc/vlan101 || cat /proc/net/vlan/vlan101) 2> /dev/null")
         self.shell.run("(cat /proc/vlan201 || cat /proc/net/vlan/vlan201) 2> /dev/null")
         self.shell.run("ip address show vlan101")
@@ -256,7 +239,7 @@ class TestbedDevice:
         network = self.args.network
         shell = self.target.get_driver("ShellDriver")
         shell.wait_for(
-            'ifstatus {} | jsonfilter -qe "@.up" || true'.format(network), "true", 60.0
+            "ubus-cli 'IP.Interface.[Alias==\"lan\"].Status?0'", "Up", 60.0
         )
 
         shell.wait_for("ping -c1 {} || true".format(host), ", 0% packet loss", 180.0)

.gitlab/scripts/dut-init/cdrouter-dut-enable-pppoe.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-ssh root@$TARGET_LAN_IP "ubus -t 200 wait_for PPP"
-ssh root@$TARGET_LAN_IP "ubus -t 200 wait_for DHCPv4"
-ssh root@$TARGET_LAN_IP "ubus -t 200 wait_for IP"
-ssh root@$TARGET_LAN_IP "ubus call DHCPv4.Client.1 _set '{\"parameters\":{\"Enable\":0}}'"
-ssh root@$TARGET_LAN_IP "ubus call PPP.Interface.1 _set '{\"parameters\":{\"Enable\":1}}'"
-ssh root@$TARGET_LAN_IP "ubus call IP.Interface.2 _set '{\"parameters\":{\"LowerLayers\":\"Device.PPP.Interface.1.\"}}'"
-ssh root@$TARGET_LAN_IP "ubus call IP.Interface.2.IPv4Address.1 _set '{\"parameters\":{\"AddressingType\":\"IPCP\"}}'"
+ssh root@$TARGET_LAN_IP "ubus -t 200 wait_for WANManager"
+ssh root@$TARGET_LAN_IP "ubus-cli WANManager.WAN.Ethernet_PPP.Intf.1.Type=\"untagged\""
+ssh root@$TARGET_LAN_IP "ubus call WANManager setWANMode '{ \"WANMode\": \"Ethernet_PPP\" }'"

commit 8928b1047cba348e48808643e689be23942c8d7a

ci: testbed-device.py: make the LAN VLAN init device agnostic
Changes done in commit 7a52da49db2b (".gitlab: scripts: Update scripts
for M1 delivery") turned generic LAN VLAN init code to URX851 specific
code so lets fix it by reverting the device specific changes.

Fixes: 7a52da49db2b (".gitlab: scripts: Update scripts for M1 delivery")
Signed-off-by: Petr Štetiar <ynezz@true.cz>

.gitlab/scripts/testbed-device.py
@@ -152,14 +152,14 @@ class TestbedDevice:
             "ubus-cli Bridging.Bridge.1.VLAN.+{Alias='vlan201', Name='vlan201', VLANID=201, Enable=1}"
         )
         self.shell.run(
-            'ubus-cli Bridging.Bridge.1.Port.6.AcceptableFrameTypes="AdmitOnlyVLANTagged"'
+            f'ubus-cli {bridge_vlan_port}.AcceptableFrameTypes="AdmitOnlyVLANTagged"'
         )
-        self.shell.run('ubus-cli Bridging.Bridge.1.Port.6.PVID="201"')
-        self.shell.run('ubus-cli Bridging.Bridge.1.Port.6.Type="CustomerVLANPort"')
-        self.shell.run("ubus-cli Bridging.Bridge.1.Port.6.Enable=1")
+        self.shell.run(f'ubus-cli {bridge_vlan_port}.PVID="201"')
+        self.shell.run(f'ubus-cli {bridge_vlan_port}.Type="CustomerVLANPort"')
+        self.shell.run(f"ubus-cli {bridge_vlan_port}.Enable=1")
 
         self.shell.run(
-            "ubus-cli Bridging.Bridge.1.VLANPort+{Alias='LAN', Name='vlan201', Port='Device.Bridging.Bridge.1.Port.6.', VLAN='Device.Bridging.Bridge.1.VLAN.1.', Enable=1}"
+            f"ubus-cli Bridging.Bridge.1.VLANPort+{{Alias='LAN', Name='vlan201', Port='Device.{bridge_vlan_port}.', VLAN='Device.Bridging.Bridge.1.VLAN.1.', Enable=1}}"
         )
 
     def init_vlans(self):