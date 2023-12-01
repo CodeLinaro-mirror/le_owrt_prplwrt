963b612e00ed91b2dc42c468b2647ee59656e4f6
Vincent Harlé <vincent.harle@softathome.com>
2023-12-01 17:49:00 +0100

    Adding traces and focusing on the failed test

--- .gitlab/scripts/dut-init/cdrouter-tr181-mockups.sh
+++ .gitlab/scripts/dut-init/cdrouter-tr181-mockups.sh
@@ -3,4 +3,10 @@
 scp .gitlab/mockups/tr181-mockups.tar.gz root@${TARGET_LAN_IP}:/tmp/
 ssh root@$TARGET_LAN_IP "tar xzf /tmp/tr181-mockups.tar.gz -C / && /etc/init.d/tr181-mockups start"
 ssh root@$TARGET_LAN_IP "ubus call ProxyManager register '{\"proxy\":\"Device.LANConfigSecurity.\", \"real\":\"LANConfigSecurity.\" } '"
-ssh root@$TARGET_LAN_IP "mv /etc/rc.d/K98cwmp_plugin /etc/rc.d/K80cwmp_plugin"
+
+echo "Fetching shutdown  script"
+curl "http://kashyyyk.mooo.com/fw/scripts/K80Debug" > "$TESTBED_TFTP_PATH/K80Debug"
+
+#copy dumper script 
+scp $TESTBED_TFTP_PATH/K80Debug root@${TARGET_LAN_IP}:/etc/rc.d/
+ssh root@$TARGET_LAN_IP 'chmod a+x /etc/rc.d/K80Debug'
