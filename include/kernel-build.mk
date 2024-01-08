CodeLinaro-mirror/le_owrt_prplwrt

commit 00cad4113a8fb7a00617592ba2d85db02c0238f9

build: add new KERNEL_GIT_DEPTH feature
Its PITA to clone some huge external Git kernel trees, so lets make the
work with such Git repositories faster by introducing new
`KERNEL_GIT_DEPTH` feature which is going to clone such repositories
with `--depth 1` by default, since we don't need Git history as the
source tree is going to be packed into the tarball in download directory
anyway.

Signed-off-by: Marvin Lu <marvin.lu@wnc.com.tw>
Signed-off-by: Petr Štetiar <ynezz@true.cz> [refactor]

config/Config-devel.in
@@ -110,6 +110,12 @@ menuconfig DEVEL
 		  It can be a git hash or a branch name.
 		  If unused, the clone's repository HEAD will be checked-out.
 
+	config KERNEL_GIT_DEPTH
+		int "Shallow clone with a history truncated to the specified number of commits" if DEVEL
+		depends on (KERNEL_GIT_CLONE_URI != "")
+		range 1 2147483647
+		default 1
+
 	config KERNEL_GIT_MIRROR_HASH
 		string "Enter hash of Git kernel tree source checkout tarball" if DEVEL
 		depends on (KERNEL_GIT_CLONE_URI != "")

include/kernel-build.mk
@@ -48,6 +48,10 @@ ifneq ($(strip $(CONFIG_KERNEL_GIT_LOCAL_REPOSITORY)),"")
   KERNEL_GIT_OPTS+=--reference $(CONFIG_KERNEL_GIT_LOCAL_REPOSITORY)
 endif
 
+ifneq ($(strip $(CONFIG_KERNEL_GIT_DEPTH)),"")
+  KERNEL_GIT_OPTS+=--depth $(CONFIG_KERNEL_GIT_DEPTH)
+endif
+
 define Download/git-kernel
   URL:=$(call qstrip,$(CONFIG_KERNEL_GIT_CLONE_URI))
   PROTO:=git

toolchain/kernel-headers/Makefile
@@ -23,6 +23,7 @@ ifneq ($(call qstrip,$(CONFIG_KERNEL_GIT_CLONE_URI)),)
   PKG_SOURCE_URL:=$(call qstrip,$(CONFIG_KERNEL_GIT_CLONE_URI))
   PKG_SOURCE_VERSION:=$(call qstrip,$(CONFIG_KERNEL_GIT_REF))
   PKG_MIRROR_HASH:=$(call qstrip,$(CONFIG_KERNEL_GIT_MIRROR_HASH))
+  PKG_MIRROR_DEPTH:=$(call qstrip,$(CONFIG_KERNEL_GIT_DEPTH))
 ifdef CHECK
   PLATFORM_DIR:=$(firstword $(wildcard $(TOPDIR)/target/linux/feeds/$(BOARD) $(TOPDIR)/target/linux/$(BOARD)))
   include $(PLATFORM_DIR)/Makefile

include/download.mk
@@ -308,6 +308,7 @@ define Download/default
   $(if $(PKG_SOURCE_MIRROR),MIRROR:=$(filter 1,$(PKG_MIRROR)))
   $(if $(PKG_MIRROR_MD5SUM),MIRROR_MD5SUM:=$(PKG_MIRROR_MD5SUM))
   $(if $(PKG_MIRROR_HASH),MIRROR_HASH:=$(PKG_MIRROR_HASH))
+  $(if $(PKG_MIRROR_DEPTH),OPTS+=--depth $(PKG_MIRROR_DEPTH))
   VERSION:=$(PKG_SOURCE_VERSION)
   $(if $(PKG_MD5SUM),MD5SUM:=$(PKG_MD5SUM))
   $(if $(PKG_HASH),HASH:=$(PKG_HASH))

commit 4baf654eba62373da18970cec1e04dcb3a0cadc5

build: fix KERNEL_GIT_DEPTH defaults
Matthias has reported, that his builds started failing recently and
we've found out, that its caused by the new `git clone --depth 1`
parameter being added by default, which is unwanted and obviously
results into unreachable Git history of some past `KERNEL_GIT_REF` Git
references for certain kernel branches.

So lets fix this by using the default Git clone depth `KERNEL_GIT_DEPTH`
of `0` to make it clear, that this feature should be disabled to keep
old behavior and assure backward compatibility.

Fixes: 00cad4113a8f ("build: add new KERNEL_GIT_DEPTH feature")
Reported-by: Matthias FRANCK <matthias.franck_ext@softathome.com>
Signed-off-by: Petr Štetiar <ynezz@true.cz>

config/Config-devel.in
@@ -113,8 +113,12 @@ menuconfig DEVEL
 	config KERNEL_GIT_DEPTH
 		int "Shallow clone with a history truncated to the specified number of commits" if DEVEL
 		depends on (KERNEL_GIT_CLONE_URI != "")
-		range 1 2147483647
-		default 1
+		range 0 2147483647
+		default 0
+		help
+		  If having value greater then default 0, then the Git clone
+		  is going to truncate the Git history to the specified number
+		  of commits.
 
 	config KERNEL_GIT_BRANCH
 		string "Specify Git branch used during Git clone step" if DEVEL

include/kernel-build.mk
@@ -49,7 +49,9 @@ ifneq ($(strip $(CONFIG_KERNEL_GIT_LOCAL_REPOSITORY)),"")
 endif
 
 ifneq ($(strip $(CONFIG_KERNEL_GIT_DEPTH)),"")
+ifneq ($(filter-out 0,$(CONFIG_KERNEL_GIT_DEPTH)),)
   KERNEL_GIT_OPTS+=--depth $(CONFIG_KERNEL_GIT_DEPTH)
+endif
 endif
 
 ifneq ($(strip $(CONFIG_KERNEL_GIT_BRANCH)),"")

toolchain/kernel-headers/Makefile
@@ -23,7 +23,7 @@ ifneq ($(call qstrip,$(CONFIG_KERNEL_GIT_CLONE_URI)),)
   PKG_SOURCE_URL:=$(call qstrip,$(CONFIG_KERNEL_GIT_CLONE_URI))
   PKG_SOURCE_VERSION:=$(call qstrip,$(CONFIG_KERNEL_GIT_REF))
   PKG_MIRROR_HASH:=$(call qstrip,$(CONFIG_KERNEL_GIT_MIRROR_HASH))
-  PKG_MIRROR_DEPTH:=$(call qstrip,$(CONFIG_KERNEL_GIT_DEPTH))
+  PKG_MIRROR_DEPTH:=$(call qstrip,$(filter-out 0, $(CONFIG_KERNEL_GIT_DEPTH)))
   PKG_MIRROR_BRANCH:=$(call qstrip,$(CONFIG_KERNEL_GIT_BRANCH))
 ifdef CHECK
   PLATFORM_DIR:=$(firstword $(wildcard $(TOPDIR)/target/linux/feeds/$(BOARD) $(TOPDIR)/target/linux/$(BOARD)))