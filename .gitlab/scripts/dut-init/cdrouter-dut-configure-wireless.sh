a107d68a418e553afb73ca7717e821243b66e2c1
Petr Štetiar <petr.stetiar@prplfoundation.org>
2024-12-19 20:49:57 +0000

    WIP: ci: cdrouter: fix interference in 2.4GHz channel 1 spectrum
    We've started seeing random issues lately with CDRouter wireless tests,
    perhaps caused by sudden increase of surrouding RF noise in the
    environment on default channel 1 in 2.4GHz band.
    
    So lets workaround that by moving the testbed configs into channel 6 for
    testbed-01 and channel 12 for testbed-02.
    
    While at it, switch from ubus-cli to ba-cli, which is now preferred due
    to its bus agnostic nature, thus making the commands more future proof.
    
    Closes: PPM-3069
    Signed-off-by: Petr Štetiar <petr.stetiar@prplfoundation.org>

--- .gitlab/scripts/dut-init/cdrouter-dut-configure-wireless.sh
+++ .gitlab/scripts/dut-init/cdrouter-dut-configure-wireless.sh
@@ -9,4 +9,12 @@ echo "$LABGRID_TARGET" | grep -q "prpl-haze" && vap_index=3
 ssh "root@$TARGET_LAN_IP" "ubus -t 200 wait_for WiFi.AccessPoint.${vap_index}"
 
 # shellcheck disable=SC2029
-ssh "root@$TARGET_LAN_IP" "ubus -S call WiFi.AccessPoint.${vap_index} _set '{\"parameters\":{\"Enable\":1}}'"
+if echo "$CI_RUNNER_DESCRIPTION" | grep -q testbed-01; then
+    export CDROUTER_CONFIG_WIFI_LAN_CHANNEL=6
+    ssh "root@$TARGET_LAN_IP" "ba-cli 'WiFi.Radio.[OperatingFrequencyBand == \"2.4GHz\"].Channel=6'"
+else
+    export CDROUTER_CONFIG_WIFI_LAN_CHANNEL=12
+    ssh "root@$TARGET_LAN_IP" "ba-cli 'WiFi.Radio.[OperatingFrequencyBand == \"2.4GHz\"].Channel=12'"
+fi
+
+ssh "root@$TARGET_LAN_IP" "ba-cli 'WiFi.Radio.[OperatingFrequencyBand == \"2.4GHz\"].Enable=1'"
