CodeLinaro-mirror/le_owrt_prplwrt

commit 95e21db42b86d2c8957ba75a711fc7318cc63b9b

ci: testbed-device.py: Mozart: fix VLAN config by adding lan0 interface
Currently the eth1 interface is being configured, but physically lan0 is
connected to the network, so lets adapt the _init_lan_vlan with correct
LAN port.

References: PCF-1462
Signed-off-by: Petr Štetiar <petr.stetiar@prplfoundation.org>
Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>

.gitlab/scripts/testbed-device.py
@@ -114,6 +114,7 @@ class TestbedDevice:
             "mxl,osp-tb341-v2": "Bridging.Bridge.1.Port.4",
             "mxl,osp-tb341": "Bridging.Bridge.1.Port.3",
             "prpl,haze": "Bridging.Bridge.1.Port.4",
+            "arcadyan,mozart": "Bridging.Bridge.1.Port.3",
             "cznic,turris-omnia": "Bridging.Bridge.2.Port.5",
             "EASY350 ANYWAN (GRX350) Axepoint Asurada model": "Bridging.Bridge.1.Port.5",
         }

commit 8219baa523e1c93e5153c65528b865db0f49e801

ci: testbed-device.py: Mozart: really fix VLAN config by adding lan0 interface
Currently we're seeing Mozart VLAN related CI test failures as it seems,
that the fixup commit fixing the bridge port of lan0 interface got lost
somehow during the rebase.

Fixes: 95e21db42b86 ("ci: testbed-device.py: Mozart: fix VLAN config by adding lan0 interface")
Signed-off-by: Petr Štetiar <petr.stetiar@prplfoundation.org>

.gitlab/scripts/testbed-device.py
@@ -114,7 +114,7 @@ class TestbedDevice:
             "mxl,osp-tb341-v2": "Bridging.Bridge.1.Port.4",
             "mxl,osp-tb341": "Bridging.Bridge.1.Port.3",
             "prpl,haze": "Bridging.Bridge.1.Port.4",
-            "arcadyan,mozart": "Bridging.Bridge.1.Port.3",
+            "arcadyan,mozart": "Bridging.Bridge.1.Port.2",
             "cznic,turris-omnia": "Bridging.Bridge.2.Port.5",
             "EASY350 ANYWAN (GRX350) Axepoint Asurada model": "Bridging.Bridge.1.Port.5",
         }