613640c32cd5fee19392b0ec9e19d406907676b7
Iacob Juc <iacob.juc_ext@softathome.com>
2025-12-01 18:58:27 +0100

    ci: cram: add test for WPA3-Compatibility Mode
    input operation :
    * Disable MLO so RSN Override 2 parameters are not present in
    hostapd.conf
    * Enable Priv VAPs; configure secMode to WPA3-CM
    * Enable MLO so RSN Override 2 parameters are present in hostapd.conf
    * Restore MLO Configuration and Security Configuration
    * Disable all VAPs to return to initial state
    
    outputs :
    * reading from pwhm the Security.ModesAvailable field and check they
    contain the WPA3-Personal-Compatibility parameter
    * reading from pwhm to see that pwhm accepts WPA3-Personal-Compatibility
    as rval for Security.ModeEnabled=X
    
    *testing an arbitrary hostapd.conf (5GHz)
    for the absence or presence of an arbitrary config parameters
    (rsn_override_key_mgmt when 11BE is disabled)
    RSNOverride 1 always present in 5GHz, never in 6GHz
    
    *testing 5GHz and 6GHz hostapd.conf for
    rsn_override_key_mgmt_2 when 11BE is enabled
    
    References: PPM-3352
    Signed-off-by: Iacob Juc <iacob.juc_ext@softathome.com>
    Signed-off-by: Houssem Dafdouf <houssem.dafdouf_ext@softathome.com>
    
    kjhdsq

--- .gitlab/tests/cram/scripts/wifi.sh
+++ .gitlab/tests/cram/scripts/wifi.sh
@@ -51,3 +51,83 @@ get_ssid_status() {
 get_ssid_ssid() {
   R "ba-cli -j -l WiFi.SSID.?0 | jsonfilter -e @[0]'[@.Alias != \"ep2g0\" && @.Alias != \"ep5g0\" && @.Alias != \"ep6g0\"].SSID'" | LC_ALL=C sort
 }
+
+# Set MLDUnit
+# In : AccessPoint object index, MLDUnit
+# Out : MLDUnit value set
+set_mlduint() {
+  R "ba-cli -l WiFi.AccessPoint.$1.SSIDReference+.MLDUnit=$2"  | sed '/^$/d'
+}
+
+# Set Radio [Arg1] OperatingStandardsFormat = [Arg2]; Standard : list of all standards; Legacy : only highest enabled 802.11 standard
+set_radio_operating_standard_format(){
+  R "ba-cli -l -j \"WiFi.Radio.[OperatingFrequencyBand=='$1'].OperatingStandardsFormat='$2'\" | jsonfilter -e @[0]'[*].OperatingStandardsFormat'"
+}
+
+# Set Radio [Arg1] OperatingStandards = [Arg2];
+set_radio_operating_standards(){
+  R "ba-cli -l -j \"WiFi.Radio.[OperatingFrequencyBand=='$1'].OperatingStandards='$2'\" | jsonfilter -e @[0]'[*].OperatingStandards'"
+}
+
+# read hostapd option from configuration file
+# Input : interface name, option
+# Output : echo option value if it exists else error message
+get_hapd_config() {
+  local target_iface="$1"
+  local target_param="$2"
+  local result
+  local target_conf_path="/tmp/${target_iface%.*}_hapd.conf"
+
+  # As when in MLO all relevant interface options are set to main link itf name, use the BSSID instead while parsing parameters
+  # this ensure the detection of the right section
+  target_bssid=$(R "ba-cli -l \"WiFi.SSID.[Name=='$itf'].BSSID?\"" | sed '/^$/d' | awk '{print toupper($0)}')
+
+  R logger -t cram "get_hapd_config: get '$target_param' of '$target_iface' with bssid '$target_bssid' from '$target_conf_path'"
+
+  result=$(R "cat $target_conf_path" 2>/dev/null | awk -v t_if="$target_bssid" -v t_pa="$target_param" '
+      BEGIN { f_sec=0; f_val=0 }
+
+      # Detect start of a section (Primary interface or BSS)
+      /^bssid=/ || /^bss=/ {
+          split($0, a, "=");
+          current_if = a[2];
+          if (current_if == t_if) { f_sec=1 }
+          next
+      }
+
+      # If inside the correct section, look for the parameter
+      f_sec == 1 && current_if == t_if {
+          # Check for exact parameter match (start of line followed by =)
+          if ($0 ~ "^" t_pa "=") {
+              split($0, b, "=");
+              print b[2];
+              f_val=1;
+              exit;
+          }
+      }
+
+      END {
+          if (f_sec == 0) { print "ERR_SECTION_NOT_FOUND"; exit 1 }
+          if (f_val == 0) { print "ERR_OPTION_NOT_FOUND"; exit 1 }
+      }
+  ')
+
+  # Handle the output and exit codes
+  case "$result" in
+      "ERR_SECTION_NOT_FOUND")
+          R logger -t cram "get_hapd_config: Interface section not found"
+          echo "Interface section not found"
+          return 0
+          ;;
+      "ERR_OPTION_NOT_FOUND")
+          R logger -t cram "get_hapd_config: Option '$target_param' not found"
+          echo "Option '$target_param' not found"
+          return 0
+          ;;
+      *)
+          R logger -t cram "get_hapd_config: $target_param='$result'"
+          echo "$result"
+          return 0
+          ;;
+  esac
+}
