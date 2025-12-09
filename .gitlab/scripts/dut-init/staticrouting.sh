4d4876a60394c2e396fe8994696508cfcd0e8b4c
Your Name <you@example.com>
2025-12-09 10:25:53 +0100

    Add remaining tests & enable configs outside of gzfile.
    Signed-off-by: Your Name <you@example.com>

--- .gitlab/scripts/dut-init/staticrouting.sh
+++ .gitlab/scripts/dut-init/staticrouting.sh
@@ -6,5 +6,14 @@ if [ $? -ne 0 ]; then
 	echo "Failed to set static route on DUT."
 	exit 1
 else
-	echo "Static route set on DUT."
+	echo "Static route set on DUT for LAN."
+fi
+
+ssh -o StrictHostKeyChecking=no root@192.168.1.1 "ip route replace 7.1.1.0/24 via 80.81.82.83"
+
+if [ $? -ne 0 ]; then
+	echo "Failed to set static route on DUT."
+	exit 1
+else
+	echo "Static route set on DUT for WAN."
 fi
