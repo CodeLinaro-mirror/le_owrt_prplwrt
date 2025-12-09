303eca67f4e74f42fb69ffa19c5e55138c6a2198
Your Name <you@example.com>
2025-12-09 13:00:39 +0100

    Temporarily skip wan tests.
    Signed-off-by: Your Name <you@example.com>

--- .gitlab/scripts/dut-init/staticrouting.sh
+++ .gitlab/scripts/dut-init/staticrouting.sh
@@ -3,17 +3,17 @@
 ssh -o StrictHostKeyChecking=no root@192.168.1.1 "ip route replace 172.16.5.0/24 via 192.168.1.150"
 
 if [ $? -ne 0 ]; then
-	echo "Failed to set static route on DUT."
+	echo "Failed to set static route on DUT for LAN."
 	exit 1
 else
 	echo "Static route set on DUT for LAN."
 fi
 
-ssh -o StrictHostKeyChecking=no root@192.168.1.1 "ip route replace 7.1.1.0/24 via 80.81.82.83"
-
-if [ $? -ne 0 ]; then
-	echo "Failed to set static route on DUT."
-	exit 1
-else
-	echo "Static route set on DUT for WAN."
-fi
+#ssh -o StrictHostKeyChecking=no root@192.168.1.1 "ip route replace 7.1.1.0/24 via 80.81.82.83"
+#
+#if [ $? -ne 0 ]; then
+#	echo "Failed to set static route on DUT for WAN."
+#	exit 1
+#else
+#	echo "Static route set on DUT for WAN."
+#fi
