DarssiniRamesh/prplwrt

ci: cram: test tr181-dhcpv4client alignment to tr181 2.19.1
Paths under test:
* DHCPv4.Client.{i}.Alias
* DHCPv4.Client.{i}.DSCPMark
* DHCPv4.Client.{i}.Interface
* DHCPv4.Client.{i}.ReqOption
* DHCPv4.Client.{i}.SentOption
* DHCPv4.Client.{i}.Stats.

References: PCF-2072
Signed-off-by: Anatolii Rybchych <anatolii.rybchych@globallogic.com>

.gitlab/tests/cram/wnc-freedom/tr181-dhcpv4client/stats.t
@@ -1,0 +1,27 @@
+Create R alias:
+
+  $ alias R="${CRAM_REMOTE_COMMAND:-}"
+
+Reset Stats:
+  $ R "ba-cli 'Device.DHCPv4.Client.1.Stats.Reset()' | grep -Ev '^>|^$'"
+  Device.DHCPv4.Client.1.Stats.Reset() returned
+  [
+      ""
+  ]
+
+Dump Stats object:
+
+  $ R "ba-cli 'dump -p Device.DHCPv4.Client.1.Stats.' | grep -Ev '^>|^$'"
+  .R...... <public>      singleton Device.DHCPv4.Client.1.Stats.
+  .R....V. <public>         uint32 Device.DHCPv4.Client.1.Stats.Inform=0
+  .R....V. <public>         uint32 Device.DHCPv4.Client.1.Stats.DiscardedPackets=0
+  .R....V. <public>         uint32 Device.DHCPv4.Client.1.Stats.Decline=0
+  .R....V. <public>         uint32 Device.DHCPv4.Client.1.Stats.OtherMessageTypes=0
+  .R....V. <public>         uint32 Device.DHCPv4.Client.1.Stats.Offer=0
+  .R....V. <public>         uint32 Device.DHCPv4.Client.1.Stats.Request=0
+  .R....V. <public>         uint32 Device.DHCPv4.Client.1.Stats.Release=0
+  .R....V. <public>         uint32 Device.DHCPv4.Client.1.Stats.TransmitFailure=0
+  .R....V. <public>         uint32 Device.DHCPv4.Client.1.Stats.ForceRenew=0
+  .R....V. <public>         uint32 Device.DHCPv4.Client.1.Stats.NACK=0
+  .R....V. <public>         uint32 Device.DHCPv4.Client.1.Stats.ACK=0
+  .R....V. <public>         uint32 Device.DHCPv4.Client.1.Stats.Discover=0

CodeLinaro-mirror/le_owrt_prplwrt

Implements: PCF-2072
References: FEAT-32
(cherry picked from commit 7d33e03403a19c2e97ea01060a8ae4b0f6e3e192)
Signed-off-by: Petr Štetiar <petr.stetiar@prplfoundation.org>

.gitlab/tests/cram/wnc-freedom/tr181-dhcpv4client/stats.t
@@ -1,0 +1,27 @@
+Create R alias:
+
+  $ alias R="${CRAM_REMOTE_COMMAND:-}"
+
+Reset Stats:
+  $ R "ba-cli 'Device.DHCPv4.Client.1.Stats.Reset()' | grep -Ev '^>|^$'"
+  Device.DHCPv4.Client.1.Stats.Reset() returned
+  [
+      ""
+  ]
+
+Dump Stats object:
+
+  $ R "ba-cli 'dump -p Device.DHCPv4.Client.1.Stats.' | grep -Ev '^>|^$'"
+  .R...... <public>      singleton Device.DHCPv4.Client.1.Stats.
+  .R....V. <public>         uint32 Device.DHCPv4.Client.1.Stats.Inform=0
+  .R....V. <public>         uint32 Device.DHCPv4.Client.1.Stats.FailedPackets=0
+  .R....V. <public>         uint32 Device.DHCPv4.Client.1.Stats.Decline=0
+  .R....V. <public>         uint32 Device.DHCPv4.Client.1.Stats.OtherMessageTypes=0
+  .R....V. <public>         uint32 Device.DHCPv4.Client.1.Stats.NAK=0
+  .R....V. <public>         uint32 Device.DHCPv4.Client.1.Stats.Offer=0
+  .R....V. <public>         uint32 Device.DHCPv4.Client.1.Stats.CorruptPackets=0
+  .R....V. <public>         uint32 Device.DHCPv4.Client.1.Stats.Request=0
+  .R....V. <public>         uint32 Device.DHCPv4.Client.1.Stats.Release=0
+  .R....V. <public>         uint32 Device.DHCPv4.Client.1.Stats.ForceRenew=0
+  .R....V. <public>         uint32 Device.DHCPv4.Client.1.Stats.ACK=0
+  .R....V. <public>         uint32 Device.DHCPv4.Client.1.Stats.Discover=0