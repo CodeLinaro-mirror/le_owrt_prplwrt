298810403a6f49588ffcaca35c61f104b6b0c3bb
Petr Štetiar <petr.stetiar@prplfoundation.org>
2026-03-25 11:18:58 +0100

    WIP: ci: disable several cram test cases for faster feedback loop
    Signed-off-by: Petr Štetiar <petr.stetiar@prplfoundation.org>

--- .gitlab/tests/cram/generic/softhsm/mqtt-publish-sec.sh
+++
@@ -1,9 +0,0 @@
-export PKCS11_MODULE_PATH=/usr/lib/softhsm/libsofthsm2.so
-mosquitto_pub \
-  --cafile /usr/share/ca-certificates/ca.crt \
-  --cert /root/certs/client.crt \
-  --key /root/certs/client.key \
-  -h prplOS.lan -p 8883 \
-  -t MySerialTopic \
-  -m "hello" \
-  -d
