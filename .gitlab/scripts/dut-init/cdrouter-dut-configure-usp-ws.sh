961ccc2e694743b247336b3840c6279370a3a99a
Piotr Nakraszewicz <piotr.nakraszewicz@consult.red>
2026-03-26 10:27:10 +0100

    ci: cdrouter: prepare separate USP MTP configs per test
    The default is STOMP.
    Add and push a separate DUT obuspa config for USP over MQTT and USP over
    WS.
    
    References: PCF-1323
    Signed-off-by: Piotr Nakraszewicz <piotr.nakraszewicz@consult.red>

---
+++ .gitlab/scripts/dut-init/cdrouter-dut-configure-usp-ws.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+scp .gitlab/scripts/dut-init/obuspa_param_reset-ws.txt root@${TARGET_LAN_IP}:/etc/config/obuspa_param_reset.txt
+ssh root@$TARGET_LAN_IP "rm /etc/obuspa.db"
+ssh root@$TARGET_LAN_IP "service obuspa restart"
