860ac20dabb190ec5f368393e714126c13d51b27
Nabil BIZID <nabil.bizid@softathome.com>
2026-06-01 10:09:11 +0200

    Revert "PROVISORY: ci: 038-lcm-pcm.t: cleanup ApplicationData resources"
    This reverts commit 693e5596d9dfe75a2d0d3869f56841533585e64e.
    
    No longer needed after fixing the original bug.
    
    References: PPW-1656
    Signed-off-by: Nabil BIZID <nabil.bizid@softathome.com>

--- .gitlab/tests/cram/generic/lcm/038-lcm-pcm.t
+++ .gitlab/tests/cram/generic/lcm/038-lcm-pcm.t
@@ -163,6 +163,3 @@ Cleanup test environment:
 
   $ R "${S} && cleanup_pcm_test"
   Done
-
-Cleanup ApplicationData volumes due to PPW-1656:
-  $ R "${S} && cleanup_appdata" > /dev/null 2>&1
--- .gitlab/tests/cram/generic/lcm/script_functions.sh
+++ .gitlab/tests/cram/generic/lcm/script_functions.sh
@@ -1025,10 +1025,6 @@ fake_fw_upgrade() {
     service rlyeh stop
     service timingila stop
 
-    umount /lcm/cthulhu/data/mounts/generic/applicationdata/mounts/00000000-0000-5000-b000-000000000001/Volume1 > /dev/null 2>&1
-    umount /lcm/cthulhu/data/mounts/generic/applicationdata/mounts/00000000-0000-5000-b000-000000000001/Volume2 > /dev/null 2>&1
-    umount /lcm/cthulhu/data/mounts/generic > /dev/null 2>&1
-
     rm -rf /etc/config/cthulhu /etc/config/lxc/"${duid}"
 
     service rlyeh start
@@ -1162,7 +1158,6 @@ add_containers_descriptors() {
         }" > /etc/amx/cthulhu/onboard/70a9bf70-9df9-5221-b51b-184c74d022e3.json
 }
 
-
 cleanup_pcm_test() {
 	remove_user_role --rolename full_caps > /dev/null
 	set_ee_roles > /dev/null
@@ -1173,18 +1168,4 @@ cleanup_pcm_test() {
 	cleanup_hostobjects
 	echo "Done"
 }
-
-cleanup_appdata() {
-	umount /lcm/cthulhu/data/mounts/generic > /dev/null 2>&1
-	umount /lcm/cthulhu/data/mounts/generic/applicationdata/mounts/00000000-0000-5000-b000-000000000001/Volume1 > /dev/null 2>&1
-	umount /lcm/cthulhu/data/mounts/generic/applicationdata/mounts/00000000-0000-5000-b000-000000000001/Volume2 > /dev/null 2>&1
-	rm -rf /lcm/cthulhu/data/mounts/generic/applicationdata
-	service cthulhu stop > /dev/null 2>&1
-	while [ -n "$(pidof cthulhu)" ]; do sleep 1; done; sleep 10
-	rm -rf /etc/config/cthulhu/* > /dev/null 2>&1
-	service cthulhu start > /dev/null
-	sleep 2
-	service timingila restart > /dev/null
-}
-
 
